65d4e3f94b7b91d4bfef14d289eb566e3015998a
István Bozsó <bozso.istvan@csfk.mta.hu>
2019-08-29 22:08:40 +0200

    Moved to Haxe

---
+++ src/gamma/Common.hx
@@ -0,0 +1,10 @@
+package gamma.common;
+
+typedef CmdFun = Array<String> -> String;
+
+function make_cmd(name: String): CmdFun {
+    return function (args: Array<String>): String {
+        return "a";
+    }
+    return inner;
+}
