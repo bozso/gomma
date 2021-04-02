da8342367deefd3b1fc26c5ad82cff322bb7fb3a
István Bozsó <bozso.istvan@epss.hu>
2021-04-02 15:14:53 +0200

    added test file

---
+++ command/debug_test.go
@@ -0,0 +1,61 @@
+package command
+
+import (
+    "testing"
+    "strings"
+    "reflect"
+    "encoding/json"
+
+    "github.com/bozso/gomma/stream"
+)
+
+const payload = `
+{
+    "debug": {
+        "type": "debug",
+        "logfile": "/tmp/test.log"
+    },
+    "default": {
+        "type": "default"
+    }
+}
+`
+
+var reference = ExecutorCreatorMap {
+    "default": Setup{},
+    "debug": DebugConfig {
+        Logfile: stream.Config {
+            Mode: stream.Path,
+            Data: "/tmp/test.log",
+        },
+    },
+}
+
+func DecodeConfigs(t *testing.T) (confs ExecutorConfigMap) {
+    dec := json.NewDecoder(strings.NewReader(payload))
+
+    if err := dec.Decode(&confs); err != nil {
+        t.Fatalf("could not decode %s into a config map: %s", payload, err)
+    }
+    return
+}
+
+func TestDecodeSetup(t *testing.T) {
+    confs := DecodeConfigs(t)
+
+    creators := make(ExecutorCreatorMap)
+
+    for key, val := range confs {
+        creator, err := val.ToCreator()
+        if err != nil {
+            t.Errorf("could not create creator: %s", err)
+        }
+
+        creators[key] = creator
+    }
+
+
+    if !reflect.DeepEqual(reference, creators) {
+        t.Errorf("expected %v and %v to be equal", confs, reference)
+    }
+}
