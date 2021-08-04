1e0f27e84a6a092be7d1535c16015dc585187dd8
István Bozsó <bozso.istvan@epss.hu>
2021-08-04 12:28:49 +0200

    merge

--- parser/getter.go
+++ parser/getter.go
@@ -1,11 +1,35 @@
 package parser
 
-import ()
+import (
+	"bufio"
+	"io"
+)
 
-type MapSetup struct {
-	splitter Splitter
+type Line interface {
+	ParseLine(line string) (err error)
+}
+
+func ProcInput(r io.Reader, l Line) (err error) {
+	s := bufio.NewScanner(r)
+	for s.Scan() {
+		if err = l.ParseLine(s.Text()); err != nil {
+			return
+		}
+	}
+
+	return s.Err()
 }
 
 type Map struct {
 	data map[string]string
+}
+
+type MapCreator struct {
+}
+
+func NewMap(r io.Reader) (m Map, err error) {
+	m = make(Map)
+
+	return ProcIntut(r, func(line string) (err error) {
+	})
 }
