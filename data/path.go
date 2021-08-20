288325672ebc1cf5363316270d004818d37b2408
István Bozsó <bozso.istvan@epss.hu>
2021-08-20 18:41:42 +0200

    cleanup of commented out code

--- data/path.go
+++ data/path.go
@@ -27,30 +27,3 @@ func (p PathWithPar) WithPar(file string) (pp PathWithPar) {
 
 	return p
 }
-
-/*
-
-func (d File) WithShape(p Path) (f File, err error) {
-	f, err = d.WithShapeDType(p, d.Dtype)
-	return
-}
-
-func (d File) WithShapeDType(p Path, dtype Type) (f File, err error) {
-	if dtype == Unknown {
-		dtype = d.Dtype
-	}
-
-	f, err = p.Load(d.RngAzi, dtype)
-	return
-}
-
-func (p Path) Load(ra common.RngAzi, dtype Type) (f File, err error) {
-	f.DataFile, err = p.DataFile.ToValidFile()
-	if err != nil {
-		return
-	}
-
-	f.RngAzi, f.Dtype = ra, dtype
-	return
-}
-*/
