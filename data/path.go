11069463ea22faffade3d8932262e468fc47e15c
István Bozsó <bozso.istvan@epss.hu>
2021-08-20 18:35:26 +0200

    cleanup and refactoring; setting up work for Loader

--- data/path.go
+++ data/path.go
@@ -1,16 +1,31 @@
 package data
 
-import (
-	"github.com/bozso/gomma/common"
-)
-
 type Path struct {
 	DataFile string `json:"data_file"`
 }
 
 func New(file string) (p Path) {
 	p.DataFile = file
+
 	return
+}
+
+func (p Path) WithParFile(file string) (pp PathWithPar) {
+	return PathWithPar{
+		Path:    p,
+		ParFile: file,
+	}
+}
+
+type PathWithPar struct {
+	Path    Path
+	ParFile string
+}
+
+func (p PathWithPar) WithPar(file string) (pp PathWithPar) {
+	p.ParFile = file
+
+	return p
 }
 
 /*
